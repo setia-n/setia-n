780225678089c09e8fa32d8915ed9ffbbb1f88b5
setia-n <94894003+setia-n@users.noreply.github.com>
2021-11-23 15:06:34 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="Content-Type"  content="text/html charset=UTF-8" />
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+	<meta name="x-apple-disable-message-reformatting">
+
+	<title>IMAJIKU</title>
+
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<style type="text/css">
+		/*typography*/
+		a{
+			color: #0080FF;
+			transition: all 0.3s ease;
+			-webkit-transition: all 0.3s ease;
+			-moz-transition: all 0.3s ease;
+		}
+		a:link {
+			color: #0080FF; 
+			text-decoration: none;
+		}
+		a:visited {
+			color: #0080FF; 
+			text-decoration: none;
+		}
+		a:hover {
+			color: #006AD5;
+		}
+		a:hover, 
+		a:active {
+			outline: 0 0;
+		}
+		a:focus{
+			outline: 0;
+		}
+		:focus {
+			outline: none;
+		}
+
+		/*general*/
+		body{
+			background-color: #f5f5f5;
+			font-size: 12px;
+			font-family: 'Arial', Helvetica, sans-serif;
+			color: #333333;
+			padding: 50px 0 50px !important;
+			margin: 0 !important;
+			width: 100% !important;
+			line-height: 1.5;
+			-webkit-text-size-adjust: 100% !important;
+			-ms-text-size-adjust: 100% !important;
+			-webkit-font-smoothing: antialiased !important;
+		}
+
+		.main_content {
+			background-color: #ccc;
+			color: #fff;
+			border: 1px dashed #111;
+			margin-bottom: 40px;
+		}
+
+		.profile {
+			width: 150px;
+		}
+
+		.profile img {
+			width: 100%;
+		}
+
+		.lazy {
+			opacity: 0;
+			transition: opacity 1s;
+		}
+	</style>
+</head>
+
+<body>
+	<section class="testimonial">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-12 lazy" data-loader="showDiv">
+					<div class="text-center main_content">
+						<div class="profile">
+							<img src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/512/user-male-icon.png">
+							<h2>Username</h2>
+							<p>Designation</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-12 lazy" data-loader="showDiv">
+					<div class="text-center main_content">
+						<div class="profile">
+							<img src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/512/user-male-icon.png">
+							<h2>Username</h2>
+							<p>Designation</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-12 lazy" data-loader="showDiv">
+					<div class="text-center main_content">
+						<div class="profile">
+							<img src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/512/user-male-icon.png">
+							<h2>Username</h2>
+							<p>Designation</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-12 lazy" data-loader="showDiv">
+					<div class="text-center main_content">
+						<div class="profile">
+							<img src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/512/user-male-icon.png">
+							<h2>Username</h2>
+							<p>Designation</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-12 lazy" data-loader="showDiv">
+					<div class="text-center main_content">
+						<div class="profile">
+							<img src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/512/user-male-icon.png">
+							<h2>Username</h2>
+							<p>Designation</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-12 lazy" data-loader="showDiv">
+					<div class="text-center main_content">
+						<div class="profile">
+							<img src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/512/user-male-icon.png">
+							<h2>Username</h2>
+							<p>Designation</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-12 lazy" data-loader="showDiv">
+					<div class="text-center main_content">
+						<div class="profile">
+							<img src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/512/user-male-icon.png">
+							<h2>Username</h2>
+							<p>Designation</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-12 lazy" data-loader="showDiv">
+					<div class="text-center main_content">
+						<div class="profile">
+							<img src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/512/user-male-icon.png">
+							<h2>Username</h2>
+							<p>Designation</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- begin jquery -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<!-- end jquery -->
+
+	<!-- begin popper js -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<!-- end popper js -->
+
+
+	<!-- begin jquery extra -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+
+	<!-- end jquery extra -->
+
+	<script src="https://cdn.jsdelivr.net/npm/zepto@1.2.0/dist/zepto.min.js"></script>
+
+
+	<script src="js/jquery.lazy.min.js"></script>
+<script src="js/main.js"></script>
+
+
+	<!-- END ADD ON / PLUGINS JS -->
+	<script>
+
+	</script>
+
+
+</body>
+
+</html>
+
+
